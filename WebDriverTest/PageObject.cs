902aeceeae33ca484a931dc08ad2ec56e3a95107
Zyglin <zyglin@mail.ru>
2018-11-15 20:33:42 +0300

    Update

--- WebDriverTest/PageObject.cs
+++ WebDriverTest/PageObject.cs
@@ -1,34 +1,27 @@
-п»їusing Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.PageObjects;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace WebDriverTest
 {
-    public class PageHome 
+    [TestFixture]
+    public class TestClass
     {
 
-        [FindsBy(How = How.XPath, Using = "//a[@class='fw700 fs-16 bold']")]
-        private IWebElement expLink;
+        private IWebDriver driver;
+        const string url = "https://www.turkishairlines.com/";
 
-        [FindsBy(How = How.XPath, Using = "//div[@id='exploreourbestofferscarousel']//div[@class='carousel-caption']//a")]
-        public IWebElement firstcity;
-
-        [FindsBy(How = How.XPath, Using = "//a[@class='btn btn-danger btn-lg pt-0 pb-0 pr-20 destination-btn-fix']//")]
-        public IWebElement buttonResult;
-
-
-        public IWebElement ExpLink { get { return expLink; } }
-        public IWebElement FirstCity { get { return firstcity; } }
-        public IWebElement ButtonResult { get { return buttonResult; } }
+        [Test]
+        public void Test_spechialoffer()
+        {
+            driver.Navigate().GoToUrl(url);
+            PageHome pageHome = new FindElements();
+            PageFactory.InitElements(driver, pageHome);
+            pageHome.ExpLink.Click();
+            pageHome.FirstCity.Click();           
+            Assert.IsNotNull(pageHome.ButtonResult, "Here is no any data about flight");
+            Assert.AreEqual("Забронировать перелёт", pageHome.ButtonResult.Text, "flight status is incorrect");
+          
+        }
     }
-
 }
