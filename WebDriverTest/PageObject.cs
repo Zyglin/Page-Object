d012947c9c4eb5b6782133986f55e2a38ab30d3f
Zyglin <zyglin@mail.ru>
2018-11-15 20:26:36 +0300

    Update PageObject.cs

--- WebDriverTest/PageObject.cs
+++ WebDriverTest/PageObject.cs
@@ -13,49 +13,22 @@ using System.Threading.Tasks;
 
 namespace WebDriverTest
 {
-    public class Main:IDisposable
+    public class PageHome 
     {
-        private  IWebDriver driver;
 
         [FindsBy(How = How.XPath, Using = "//a[@class='fw700 fs-16 bold']")]
-        public IWebElement ExpLink;
+        private IWebElement expLink;
 
         [FindsBy(How = How.XPath, Using = "//div[@id='exploreourbestofferscarousel']//div[@class='carousel-caption']//a")]
-        public IWebElement Firstcity;
+        public IWebElement firstcity;
 
         [FindsBy(How = How.XPath, Using = "//a[@class='btn btn-danger btn-lg pt-0 pb-0 pr-20 destination-btn-fix']//")]
-        public IWebElement ButtonResult;
+        public IWebElement buttonResult;
 
 
-
-        public Main()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
-            
-            PageFactory.InitElements(driver, this);
-        }
-
-      
-
-        public void Search()
-        {
-            driver.Navigate().GoToUrl("https://www.turkishairlines.com/");
-            var wait = new WebDriverWait(driver,TimeSpan.FromSeconds(15));
-            ExpLink.Click();
-            Firstcity.Click();
-            if(ButtonResult != null)
-            {
-                Assert.AreEqual("Забронировать перелёт", ButtonResult.Text);
-            }
-
-        }
-
-        public void Dispose()
-        {
-            driver.Quit();
-            driver = null;
-        }
+        public IWebElement ExpLink { get { return expLink; } }
+        public IWebElement FirstCity { get { return firstcity; } }
+        public IWebElement ButtonResult { get { return buttonResult; } }
     }
-   
+
 }
